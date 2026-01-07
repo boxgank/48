5ccd89b83a34fb043599f3675f246aeb7b3f218d
lalalily <149517952+boxgank@users.noreply.github.com>
2026-01-07 17:05:10 +0700

    Update sorter.js

--- sorter.js
+++ sorter.js
@@ -1,23 +1,132 @@
 const members = [
-  {
-    id: "nur_intan",
-    name: "Nur Intan",
-    gen: 10,
-    team: "J"
-  },
-  {
-    id: "abigail",
-    name: "Abigail Rachel",
-    gen: 10,
-    team: "KIII"
-  },
-  {
-    id: "kimberly",
-    name: "Victoria Kimberly",
-    gen: 9,
-    team: "J"
-  }
+  { id: "alya_amanda", name: "Alya Amanda", gen: 11, team: "Love", img: "https://jkt48.com/profile/alya_amanda.jpg" },
+  { id: "anindya_ramadhani", name: "Anindya Ramadhani", gen: 11, team: "Love", img: "https://jkt48.com/profile/anindya_ramadhani.jpg" },
+  { id: "aurellia", name: "Aurellia", gen: 11, team: "Love", img: "https://jkt48.com/profile/aurellia.jpg" },
+  { id: "aurhel_alana", name: "Aurhel Alana", gen: 11, team: "Love", img: "https://jkt48.com/profile/aurhel_alana.jpg" },
+  { id: "cathleen_nixie", name: "Cathleen Nixie", gen: 11, team: "Love", img: "https://jkt48.com/profile/cathleen_nixie.jpg" },
+  { id: "celline_thefani", name: "Celline Thefani", gen: 11, team: "Love", img: "https://jkt48.com/profile/celline_thefani.jpg" },
+  { id: "cynthia_yaputera", name: "Cynthia Yaputera", gen: 11, team: "Love", img: "https://jkt48.com/profile/cynthia_yaputera.jpg" },
+  { id: "fiony_alveria", name: "Fiony Alveria", gen: 11, team: "Love", img: "https://jkt48.com/profile/fiony_alveria.jpg" },
+  { id: "fritzy_rosmerian", name: "Fritzy Rosmerian", gen: 11, team: "Love", img: "https://jkt48.com/profile/fritzy_rosmerian.jpg" },
+  { id: "grace_octaviani", name: "Grace Octaviani", gen: 11, team: "Love", img: "https://jkt48.com/profile/grace_octaviani.jpg" },
+  { id: "hillary_abigail", name: "Hillary Abigail", gen: 11, team: "Love", img: "https://jkt48.com/profile/hillary_abigail.jpg" },
+  { id: "indah_cahya", name: "Indah Cahya", gen: 11, team: "Love", img: "https://jkt48.com/profile/indah_cahya.jpg" },
+  { id: "jazzlyn_trisha", name: "Jazzlyn Trisha", gen: 11, team: "Love", img: "https://jkt48.com/profile/jazzlyn_trisha.jpg" },
+  { id: "michelle_alexandra", name: "Michelle Alexandra", gen: 11, team: "Love", img: "https://jkt48.com/profile/michelle_alexandra.jpg" },
+  { id: "nayla_suji", name: "Nayla Suji", gen: 11, team: "Love", img: "https://jkt48.com/profile/nayla_suji.jpg" },
+
+  { id: "adeline_wijaya", name: "Adeline Wijaya", gen: 10, team: "Dream", img: "https://jkt48.com/profile/adeline_wijaya.jpg" },
+  { id: "amanda_sukma", name: "Amanda Sukma", gen: 10, team: "Dream", img: "https://jkt48.com/profile/amanda_sukma.jpg" },
+  { id: "chelsea_davina", name: "Chelsea Davina", gen: 10, team: "Dream", img: "https://jkt48.com/profile/chelsea_davina.jpg" },
+  { id: "febriola_sinambela", name: "Febriola Sinambela", gen: 10, team: "Dream", img: "https://jkt48.com/profile/febriola_sinambela.jpg" },
+  { id: "freya_jayawardana", name: "Freya Jayawardana", gen: 8, team: "Dream", img: "https://jkt48.com/profile/freya_jayawardana.jpg" },
+  { id: "gabriela_abigail", name: "Gabriela Abigail", gen: 10, team: "Dream", img: "https://jkt48.com/profile/gabriela_abigail.jpg" },
+  { id: "gendis_mayrannisa", name: "Gendis Mayrannisa", gen: 10, team: "Dream", img: "https://jkt48.com/profile/gendis_mayrannisa.jpg" },
+  { id: "gita_sekar_andarini", name: "Gita Sekar Andarini", gen: 10, team: "Dream", img: "https://jkt48.com/profile/gita_sekar_andarini.jpg" },
+  { id: "greesela_adhalia", name: "Greesela Adhalia", gen: 10, team: "Dream", img: "https://jkt48.com/profile/greesela_adhalia.jpg" },
+  { id: "helisma_putri", name: "Helisma Putri", gen: 10, team: "Dream", img: "https://jkt48.com/profile/helisma_putri.jpg" },
+  { id: "jesslyn_elly", name: "Jesslyn Elly", gen: 10, team: "Dream", img: "https://jkt48.com/profile/jesslyn_elly.jpg" },
+  { id: "marsha_lenathea", name: "Marsha Lenathea", gen: 9, team: "Dream", img: "https://jkt48.com/profile/marsha_lenathea.jpg" },
+  { id: "nina_tutachia", name: "Nina Tutachia", gen: 10, team: "Dream", img: "https://jkt48.com/profile/nina_tutachia.jpg" },
+  { id: "oline_manuel", name: "Oline Manuel", gen: 7, team: "Dream", img: "https://jkt48.com/profile/oline_manuel.jpg" },
+  { id: "shabilqis_naila", name: "Shabilqis Naila", gen: 10, team: "Dream", img: "https://jkt48.com/profile/shabilqis_naila.jpg" },
+
+  { id: "abigail_rachel", name: "Abigail Rachel", gen: 10, team: "Passion", img: "https://jkt48.com/profile/abigail_rachel.jpg" },
+  { id: "angelina_christy", name: "Angelina Christy", gen: 7, team: "Passion", img: "https://jkt48.com/profile/angelina_christy.jpg" },
+  { id: "catherina_vallencia", name: "Catherina Vallencia", gen: 10, team: "Passion", img: "https://jkt48.com/profile/catherina_vallencia.jpg" },
+  { id: "cornelia_vanisa", name: "Cornelia Vanisa", gen: 10, team: "Passion", img: "https://jkt48.com/profile/cornelia_vanisa.jpg" },
+  { id: "dena_natalia", name: "Dena Natalia", gen: 10, team: "Passion", img: "https://jkt48.com/profile/dena_natalia.jpg" },
+  { id: "desy_natalia", name: "Desy Natalia", gen: 10, team: "Passion", img: "https://jkt48.com/profile/desy_natalia.jpg" },
+  { id: "feni_fitriyanti", name: "Feni Fitriyanti", gen: 3, team: "Passion", img: "https://jkt48.com/profile/feni_fitriyanti.jpg" },
+  { id: "jessica_chandra", name: "Jessica Chandra", gen: 7, team: "Passion", img: "https://jkt48.com/profile/jessica_chandra.jpg" },
+  { id: "kathrina_irene", name: "Kathrina Irene", gen: 9, team: "Passion", img: "https://jkt48.com/profile/kathrina_irene.jpg" },
+  { id: "lulu_salsabila", name: "Lulu Salsabila", gen: 8, team: "Passion", img: "https://jkt48.com/profile/lulu_salsabila.jpg" },
+  { id: "michelle_levia", name: "Michelle Levia", gen: 10, team: "Passion", img: "https://jkt48.com/profile/michelle_levia.jpg" },
+  { id: "mutiara_azzahra", name: "Mutiara Azzahra", gen: 7, team: "Passion", img: "https://jkt48.com/profile/mutiara_azzahra.jpg" },
+  { id: "raisha_syifa", name: "Raisha Syifa", gen: 10, team: "Passion", img: "https://jkt48.com/profile/raisha_syifa.jpg" },
+  { id: "ribka_budiman", name: "Ribka Budiman", gen: 10, team: "Passion", img: "https://jkt48.com/profile/ribka_budiman.jpg" },
+  { id: "victoria_kimberly", name: "Victoria Kimberly", gen: 9, team: "Passion", img: "https://jkt48.com/profile/victoria_kimberly.jpg" }
 ];
+
+const genBox = document.getElementById("genSelect");
+const teamBox = document.getElementById("teamSelect");
+
+function renderFilters() {
+  const gens = [...new Set(members.map(m => m.gen))].sort((a,b)=>a-b);
+  const teams = [...new Set(members.map(m => m.team))];
+
+  genBox.innerHTML = "";
+  teamBox.innerHTML = "";
+
+  gens.forEach(g => {
+    genBox.innerHTML += `<h4>Generasi ${g}</h4>`;
+    members.filter(m => m.gen === g).forEach(m => {
+      genBox.innerHTML += `
+        <label>
+          <input type="checkbox" value="${m.id}">
+          ${m.name}
+        </label><br>
+      `;
+    });
+  });
+
+  teams.forEach(t => {
+    teamBox.innerHTML += `<h4>Team ${t}</h4>`;
+    members.filter(m => m.team === t).forEach(m => {
+      teamBox.innerHTML += `
+        <label>
+          <input type="checkbox" value="${m.id}">
+          ${m.name}
+        </label><br>
+      `;
+    });
+  });
+}
+
+renderFilters();
+
+document.querySelectorAll('input[name="mode"]').forEach(radio => {
+  radio.addEventListener("change", e => {
+    genBox.style.display = e.target.value === "gen" ? "block" : "none";
+    teamBox.style.display = e.target.value === "team" ? "block" : "none";
+  });
+});
+
+function startFromSelection() {
+  const mode = document.querySelector('input[name="mode"]:checked').value;
+  let selected = [];
+
+  if (mode === "all") {
+    selected = members;
+  } else {
+    const checked = document.querySelectorAll(
+      '#selectScreen input[type="checkbox"]:checked'
+    );
+
+    const ids = new Set();
+    checked.forEach(c => ids.add(c.value));
+
+    selected = members.filter(m => ids.has(m.id));
+  }
+
+  if (selected.length < 2) {
+    alert("Pilih minimal 2 member");
+    return;
+  }
+
+  document.getElementById("selectScreen").style.display = "none";
+  document.getElementById("sorterScreen").style.display = "block";
+
+  initSorter(selected);
+}
+
+function initSorter(selectedMembers) {
+  lists = selectedMembers.map(m => [m]);
+  shuffle(lists);
+  total = Math.ceil(selectedMembers.length * Math.log2(selectedMembers.length));
+  current = 0;
+  nextMerge();
+}
 
 
 let lists = [];
@@ -31,15 +140,6 @@ let ri = 0;
 let total = 0;
 let current = 0;
 
-document.querySelectorAll('input[name="mode"]').forEach(radio => {
-  radio.addEventListener("change", e => {
-    document.getElementById("genSelect").style.display =
-      e.target.value === "gen" ? "block" : "none";
-
-    document.getElementById("teamSelect").style.display =
-      e.target.value === "team" ? "block" : "none";
-  });
-});
 
 function renderGenList() {
   const gens = [...new Set(members.map(m => m.gen))];
