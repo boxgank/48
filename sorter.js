28b8ba19790c2a8079bb633e2f367a32febd3e30
lalalily <149517952+boxgank@users.noreply.github.com>
2026-01-07 16:51:50 +0700

    Update sorter.js

--- sorter.js
+++ sorter.js
@@ -31,6 +31,86 @@ let ri = 0;
 let total = 0;
 let current = 0;
 
+document.querySelectorAll('input[name="mode"]').forEach(radio => {
+  radio.addEventListener("change", e => {
+    document.getElementById("genSelect").style.display =
+      e.target.value === "gen" ? "block" : "none";
+
+    document.getElementById("teamSelect").style.display =
+      e.target.value === "team" ? "block" : "none";
+  });
+});
+
+function renderGenList() {
+  const gens = [...new Set(members.map(m => m.gen))];
+  const box = document.getElementById("genSelect");
+  box.innerHTML = "";
+
+  gens.forEach(g => {
+    const list = members.filter(m => m.gen === g);
+    box.innerHTML += `<h4>Generasi ${g}</h4>`;
+    list.forEach(m => {
+      box.innerHTML += `
+        <label>
+          <input type="checkbox" value="${m.id}">
+          ${m.name}
+        </label><br>
+      `;
+    });
+  });
+}
+
+function renderTeamList() {
+  const teams = [...new Set(members.map(m => m.team))];
+  const box = document.getElementById("teamSelect");
+  box.innerHTML = "";
+
+  teams.forEach(t => {
+    const list = members.filter(m => m.team === t);
+    box.innerHTML += `<h4>Team ${t}</h4>`;
+    list.forEach(m => {
+      box.innerHTML += `
+        <label>
+          <input type="checkbox" value="${m.id}">
+          ${m.name}
+        </label><br>
+      `;
+    });
+  });
+}
+
+function confirmSelection() {
+  const mode = document.querySelector('input[name="mode"]:checked').value;
+  let selected = [];
+
+  if (mode === "all") {
+    selected = members;
+  } else {
+    const checked = document.querySelectorAll(
+      '#selectScreen input[type="checkbox"]:checked'
+    );
+
+    const ids = new Set();
+    checked.forEach(c => ids.add(c.value));
+
+    selected = members.filter(m => ids.has(m.id));
+  }
+
+  if (selected.length < 2) {
+    alert("Pilih minimal 2 member");
+    return;
+  }
+
+  startSorterWith(selected);
+}
+
+function startSorterWith(selectedMembers) {
+  document.getElementById("selectScreen").style.display = "none";
+  document.getElementById("sorterScreen").style.display = "block";
+  initSorter(selectedMembers);
+}
+
+
 function startSorter() {
   lists = members.map(m => [m]);
   shuffle(lists);
