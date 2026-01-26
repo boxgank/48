d377e316eb21274bfe550914f84adccb63d78741
lalalily <149517952+boxgank@users.noreply.github.com>
2026-01-27 01:16:21 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 // js/supabase.js
-const supabase = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
   "https://fyumvtdufxfkivwcuwsy.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5dW12dGR1Znhma2l2d2N1d3N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzNDkxNDYsImV4cCI6MjA4NDkyNTE0Nn0.AP2En4VfC-eayvokOzR6QAUm6SIwfTX5pJVgZ_kKxhA"
 );
